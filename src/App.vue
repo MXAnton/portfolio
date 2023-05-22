335285870a64d1cb490197d3836b6acf4873aad7
Anton Lehrberg <lehrberg.anton@gmail.com>
2023-05-22 13:34:52 +0200

    Init project to git.

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+export default {
+  data() {
+    return {
+      menuActive: false
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <RouterLink to="/">
+      <img alt="My logo" src="@/assets/logo.svg" width="32" height="32" />
+    </RouterLink>
+
+    <button id="hamburger-menu__button" @click="menuActive = !menuActive">
+      <img alt="Hamburger menu" src="@/assets/HamburgerMenu.svg" width="32" height="32" />
+    </button>
+
+    <nav id="nav-menu" :class="{ active: menuActive }">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/#tools">Tools</RouterLink>
+      <RouterLink to="/#portfolio">Portfolio</RouterLink>
+      <RouterLink to="/#contact">Contact</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+
+  <footer>
+    <p>Copyright © {{ new Date().getFullYear() }} Anton Lehrberg All Rights Reserved</p>
+  </footer>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+
+  padding: 32px var(--horizontal--padding);
+
+  z-index: 2;
+}
+
+#hamburger-menu__button {
+  border: none;
+  background: none;
+
+  z-index: 2;
+}
+
+#nav-menu {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  width: 100vw;
+
+  padding: 50px 20px;
+
+  background-color: var(--blue--lighter);
+
+  transform-origin: top;
+}
+#nav-menu:not(.active) {
+  animation: nav-menu-fadeout 0.2s forwards ease-in-out;
+}
+#nav-menu.active {
+  animation: nav-menu-fadein 0.2s forwards ease-in-out;
+}
+@keyframes nav-menu-fadein {
+  from {
+    transform: scaleY(0) translateY(-20%);
+    opacity: 0;
+  }
+  to {
+    transform: scaleY(1) translateY(0);
+    opacity: 1;
+  }
+}
+@keyframes nav-menu-fadeout {
+  from {
+    transform: scaleY(1) translateY(0);
+  }
+  to {
+    transform: scaleY(0) translateY(-20%);
+    opacity: 0;
+  }
+}
+
+#nav-menu > a {
+  color: var(--black);
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,98 @@
+<script></script>
+
+<template>
+  <main>
+    <section id="hero">
+      <h1>ANTON LEHRBERG</h1>
+      <h3>FULL STACK DEVELOPER</h3>
+      <h4>from <span>SWEDEN</span></h4>
+    </section>
+
+    <section id="main-tools">
+      <h2>MAIN TOOLS<span class="small">:</span></h2>
+
+      <div class="tools-wrapper">
+        <div class="tool">HTML</div>
+        <div class="tool">CSS</div>
+        <div class="tool">JS</div>
+        <div class="tool">Vue.js</div>
+        <div class="tool">Node.js</div>
+        <div class="tool">Express.js</div>
+        <div class="tool">SQL</div>
+        <div class="tool">Figma</div>
+      </div>
+    </section>
+
+    <section id="portfolio">
+      <h2>PORTFOLIO</h2>
+      <p>I have made several websites, of which many are in professional use as you read this.</p>
+
+      <div class="examples">
+        <div class="example">
+          <img src="" alt="Display of homepage of site" />
+
+          <div class="example__content">
+            <a
+              href="https://www.calmprofessor.se/"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="link-1"
+              >calmprofessor.se</a
+            >
+            <p>Take quick online courses where ever you are!</p>
+
+            <div class="tools-wrapper">
+              <div class="tool">HTML</div>
+              <div class="tool">CSS</div>
+              <div class="tool">JS</div>
+              <div class="tool">Vue.js</div>
+              <div class="tool">Node.js</div>
+              <div class="tool">Express.js</div>
+              <div class="tool">SQL</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="example">
+          <img src="" alt="Display of homepage of site" />
+
+          <div class="example__content">
+            <a href="https://www.mhfothem.se/" target="_blank" rel="noopener noreferrer" class="link-1">mhfothem.se</a>
+            <p>
+              Motocross association that gives everyone the opportunity to ride motocross, with main
+              focus on children.
+            </p>
+
+            <div class="tools-wrapper">
+              <div class="tool">HTML</div>
+              <div class="tool">CSS</div>
+              <div class="tool">JS</div>
+              <div class="tool">Vue.js</div>
+              <div class="tool">Google Firestore & Auth</div>
+              <div class="tool">Figma</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="contact">
+      <h2>HIT ME UP</h2>
+
+      <form action="">
+        <input type="email" name="email" id="email" placeholder="Your email..." required />
+        <textarea
+          name="text"
+          id="text"
+          cols="30"
+          rows="10"
+          placeholder="Write me something..."
+          required
+        ></textarea>
+      </form>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+</style>
