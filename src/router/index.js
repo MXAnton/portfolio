72c8e94f13dd312f55ac510ef12ae88b77b9be98
Anton Lehrberg <lehrberg.anton@gmail.com>
2024-04-04 12:14:32 +0200

    Added 404 page.

--- src/router/index.js
+++ src/router/index.js
@@ -23,6 +23,12 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/ThanksView.vue')
+    },
+
+    {
+      path: '/:catchAll(.*)*',
+      name: 'notFound',
+      component: () => import('../views/NotFoundView.vue')
     }
   ],
   scrollBehavior(to, from, savedPosition) {
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,30 @@
+<template>
+  <main>
+    <section>
+      <div class="wrapper">
+        <div class="header">
+          <h1>404</h1>
+          <p>Page not found</p>
+        </div>
+
+        <RouterLink to="/" class="button-1">Back to home</RouterLink>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script></script>
+
+<style scoped>
+h1 {
+  font-size: 8rem;
+}
+p {
+  font-size: 2rem;
+}
+
+.button-1 {
+  width: fit-content;
+  padding: 0.8em 1.5em;
+}
+</style>
